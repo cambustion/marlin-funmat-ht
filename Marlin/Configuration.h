47372e990fa1b1a119707dda5a355a6eff364bc1
ramblehead <v.rybynok@gmail.com>
2023-03-28 18:11:02 +0100

    Checkpoint

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2195,7 +2195,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (30*60), (30*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (25*60), (25*60), (10*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
